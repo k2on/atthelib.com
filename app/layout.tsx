ee8f00c1de4bba7dfcef0ddf9af65e4d586eacd8
Max Koon <k2@37.253.21.198.tigernet.wifi.dyn.clemson.edu>
2023-11-29 01:16:26 -0500

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        {process.env.NODE_ENV == "production" && (
+        <><Script id="ganalytics" src="https://www.googletagmanager.com/gtag/js?id=G-7ZGZMGWYW1" />
+        <Script id="ganalytics-config">
+            {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-7ZGZMGWYW1');`}
+        </Script></>)}
       <body className={inter.className}>{children}</body>
     </html>
   )
