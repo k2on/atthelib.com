8eb303cdeeca1e3787566d6623708115ff8448f7
Max Koon <theakman96@gmail.com>
2024-08-29 13:07:03 -0400

    feat: track image loading

--- app/img/[floor]/route.tsx
+++ app/img/[floor]/route.tsx
@@ -1,6 +1,8 @@
 import sharp from "sharp";
 import path from 'path';
 import { NextRequest, NextResponse } from "next/server";
+import { track } from "@vercel/analytics/server";
+import { waitUntil } from "@vercel/functions";
 
 export const dynamic = 'force-dynamic' // defaults to force-static
 
@@ -8,18 +10,18 @@ export async function GET(request: NextRequest) {
     const parts = request.url.split("/img/")[1].split(":");
     const floor = parts[0];
     
-
     let x;
     let y;
 
     const basePath = path.join(process.cwd(), `public/${floor}.png`);
     const img = sharp(basePath);
 
-
     if (parts.length > 1) {
 
         x = parseInt(parts[1]);
         y = parseInt(parts[2]);
+
+        waitUntil(track("loadImage", { floor, position: `${x},${y}` }));
 
         const overlayWidth = 100; // Width of the overlay image in pixels
         const overlayHeight = 100; // Height of the overlay image in pixels
