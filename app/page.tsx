5aed199705e3db2fd9ab772d790ba0884df445bb
anabasis <148638929+anabasisgo@users.noreply.github.com>
2024-09-05 17:18:50 -0400

    feat: add code logic and simplify main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="">
-        <ul className='min-h-screen  flex flex-col items-center justify-between floor-list'>
-        <Floor floor="6" />
-        <Floor floor="5" />
-        <Floor floor="4" />
-        <Floor floor="3" />
-        <Floor floor="2" />
-        <Floor floor="1" />
+    <main>
+        <ul className="min-h-screen flex flex-col items-center justify-between floor-list">
+			{Array(6).fill(null).map((_, i) => 
+				<Floor floor={(i + 1).toString()} key={i} />
+			)}
         </ul>
     </main>
   )
